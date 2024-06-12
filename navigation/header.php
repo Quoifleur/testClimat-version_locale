f1c8f26da57de660a8c162f72c70a6ebcc55fd14
Quoifleur <quoifleurr@gmail.com>
2024-06-12 15:24:51 +0200

    ajustement taille image témoin de connexion

--- navigation/header.php
+++ navigation/header.php
@@ -2,7 +2,7 @@
     <a href="index.php" id="header">TestClimat</a>
     <?php
     if (isset($_COOKIE['logged'])) {
-        echo '<img src="favicon.png"/> ';
+        echo '<img class"logged_picture" src="favicon.png"/> ';
     }
     ?>
 </header>
